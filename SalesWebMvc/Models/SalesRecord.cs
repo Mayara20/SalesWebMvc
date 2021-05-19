fe2fdbf6fc1565d728061722c4741e6d334fcf8d
Mayara Meireles <mayarameirelessantos@gmail.com>
2021-05-19 20:21:54 -0300

    Implementado SimpleSearch e GroupingSearch

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -15,7 +15,7 @@ namespace SalesWebMvc.Models
 		[DataType(DataType.Date)]
 		public DateTime Date { get; set; }
 
-		[DisplayFormat(DataFormatString = "{0: F2}")]
+		[DisplayFormat(DataFormatString = "{0:F2}")]
 		public double Amount { get; set; }
 		public SaleStatus Status { get; set; }
 		public Seller Seller { get; set; }
---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+
+namespace SalesWebMvc.Services
+{
+	public class SalesRecordService
+	{
+		private readonly SalesWebMvcContext _context;
+		public SalesRecordService(SalesWebMvcContext context)
+		{
+			_context = context;
+		}
+		public async Task<List<SalesRecord>> FindByDateasync(DateTime? minDate, DateTime? maxDate)
+		{
+			var result = from obj in _context.SalesRecord select obj;
+			if (minDate.HasValue)
+			{
+				result = result.Where(x => x.Date >= minDate.Value);
+			}
+			if (maxDate.HasValue)
+			{
+				result = result.Where(x => x.Date <= maxDate.Value);
+			}
+			return await result
+				.Include(x => x.Seller)
+				.Include(x => x.Seller.Department)
+				.OrderBy(x => x.Id)
+				.ToListAsync();
+		}
+		public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingasync(DateTime? minDate, DateTime? maxDate)
+		{
+			var result = from obj in _context.SalesRecord select obj;
+			if (minDate.HasValue)
+			{
+				result = result.Where(x => x.Date >= minDate.Value);
+			}
+			if (maxDate.HasValue)
+			{
+				result = result.Where(x => x.Date <= maxDate.Value);
+			}
+			return await result
+				.Include(x => x.Seller)
+				.Include(x => x.Seller.Department)
+				.OrderBy(x => x.Id)
+				.GroupBy(x => x.Seller.Department)
+				.ToListAsync();
+		}
+	}
+}
